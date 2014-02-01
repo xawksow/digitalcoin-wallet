27bf25538a4c6ec072d0912ed63c72dec2569c3e
Andreas Schildbach <andreas@schildbach.de>
2014-02-01 17:49:10 +0100

    Fix focus handling in send and request coins dialogs

--- wallet/src/de/schildbach/wallet/ui/SendCoinsFragment.java
+++ wallet/src/de/schildbach/wallet/ui/SendCoinsFragment.java
@@ -223,7 +223,7 @@ public final class SendCoinsFragment extends SherlockFragment
 		public void onDestroyActionMode(final ActionMode mode)
 		{
 			if (receivingStaticView.hasFocus())
-				amountCalculatorLink.requestFocus();
+				requestFocusFirst();
 		}
 
 		private void handleEditAddress()
@@ -240,7 +240,7 @@ public final class SendCoinsFragment extends SherlockFragment
 
 			updateView();
 
-			receivingAddressView.requestFocus();
+			requestFocusFirst();
 		}
 	}
 
@@ -250,14 +250,6 @@ public final class SendCoinsFragment extends SherlockFragment
 		public void changed()
 		{
 			updateView();
-		}
-
-		@Override
-		public void done()
-		{
-			updateView();
-
-			viewGo.requestFocusFromTouch();
 		}
 
 		@Override
@@ -440,8 +432,12 @@ public final class SendCoinsFragment extends SherlockFragment
 
 				if (everythingValid())
 					handleGo();
+				else
+					requestFocusFirst();
 			}
 		});
+
+		amountCalculatorLink.setNextFocusId(viewGo.getId());
 
 		viewCancel = (Button) view.findViewById(R.id.send_coins_cancel);
 		viewCancel.setOnClickListener(new OnClickListener()
@@ -954,6 +950,25 @@ public final class SendCoinsFragment extends SherlockFragment
 
 		if (scanAction != null)
 			scanAction.setEnabled(state == State.INPUT);
+
+		// focus linking
+		final int activeAmountViewId = amountCalculatorLink.activeTextView().getId();
+		receivingAddressView.setNextFocusDownId(activeAmountViewId);
+		receivingStaticView.setNextFocusDownId(activeAmountViewId);
+		GenericUtils.setNextFocusForwardId(receivingAddressView, activeAmountViewId);
+		viewGo.setNextFocusUpId(activeAmountViewId);
+	}
+
+	private void requestFocusFirst()
+	{
+		if (validatedAddress == null)
+			receivingAddressView.requestFocus();
+		else if (!isAmountValid())
+			amountCalculatorLink.requestFocus();
+		else if (everythingValid())
+			viewGo.requestFocus();
+		else
+			log.warn("unclear focus");
 	}
 
 	private boolean everythingValid()
@@ -1024,15 +1039,11 @@ public final class SendCoinsFragment extends SherlockFragment
 		if (paymentIntent.hasAmount())
 			amountCalculatorLink.setBtcAmount(paymentIntent.amount);
 
-		// focus
-		if (!paymentIntent.hasAmount())
-			amountCalculatorLink.requestFocus();
-		else
-			viewGo.requestFocus();
-
 		bluetoothAck = null;
 
 		updateView();
+
+		requestFocusFirst();
 
 		handler.postDelayed(new Runnable()
 		{
